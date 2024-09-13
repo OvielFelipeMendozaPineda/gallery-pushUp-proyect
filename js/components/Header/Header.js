de2080b4ae1db7529094c73a97819427f44ab9a0
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-09-13 15:46:08 -0500

    Feat: Header res.

---
+++ js/components/Header/Header.js
@@ -0,0 +1,37 @@
+class Header extends HTMLElement {
+    constructor() {
+        super()
+        this.render()
+        this.showBar()
+    }
+
+    render() {
+        this.innerHTML = /* html */ `
+        <style rel="stylesheet">
+          @import "./js/components/Header/style.css";
+        </style>
+        <header>
+        <div class="site-name">
+            <h1> <a href="/#"> Art Museum</a> </h1>
+        </div>
+        <div class="links">
+            <a href="/Exhibitions" class="link"> Exhibitions  </a>
+            <a href="/Gallery" class="link"> Gallery </a>
+            <a href="/Events" class="link"> Events  </a>
+            <a href="/Visit" class="link"> Visit </a>
+        </div>
+        <i class='bx bx-menu'></i>
+        </header>
+        `
+    }
+    showBar() {
+        const links = document.querySelector('.links')
+        const btn = document.querySelector('.bx-menu').addEventListener('click', () => {
+            links.classList.toggle('active')
+            
+        })
+    }
+}
+
+customElements.define("custom-header", Header)
+export default Header;
