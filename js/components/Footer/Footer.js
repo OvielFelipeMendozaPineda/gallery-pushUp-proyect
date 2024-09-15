b0a72db085dcfbf409f2b453999a419da4a8662a
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-09-14 20:05:49 -0500

    Feat: :sparkles: Footer component.

--- js/components/Footer/Footer.js
+++ js/components/Footer/Footer.js
@@ -9,7 +9,69 @@ class Footer extends HTMLElement {
         <style>
             @import "./js/components/Footer/Footer.css";
         </style>
-        
+            <footer id="Visit">
+        <h2 class="footer-title"> Plan your visit </h2>
+        <div class="footer-items">
+            <div class="item">
+                <div class="item-icon">
+                    <i class='bx bx-time-five'></i>
+                </div>
+                <div class="item-title">
+                    <h2> Opening hours </h2>
+                </div>
+                <div class="item-info">
+                    <p>
+                        Tuesday - Sunday 10am - 5pm
+                    </p>
+                </div>
+                <div class="item-compl">
+                    <p>Closed on Mondays</p>
+                </div>
+            </div>
+            <div class="item">
+                <div class="item-icon">
+                    <i class='bx bxs-edit-location' ></i>
+                </div>
+                <div class="item-title">
+                    <h2>  Location </h2>
+                </div>
+                <div class="item-info">
+                    <p>
+                        123 Art Street
+                    </p>
+                </div>
+                <div class="item-compl">
+                    <p>CityVille, State 12345</p>
+                </div>
+            </div>
+            <div class="item">
+                <div class="item-icon">
+                    <i class='bx bx-image-alt' ></i>
+                </div>
+                <div class="item-title">
+                    <h2>  Current Exhibition </h2>
+                </div>
+                <div class="item-info">
+                    <p>
+                        "Modern Master"
+                    </p>
+                </div>
+                <div class="item-compl">
+                    <p>Until September 30, 2023</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="copyright">
+        <p>@2024 CampusLands. All Rights Reserved</p>
+        <div class="copy-group">
+            <p> Terms Of Service</p>
+            <p> Privacy</p>
+        </div>
+    </div>
         ` 
     }
 }
+
+
+customElements.define('custom-footer', Footer)
--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 import './components/Header/Header.js'
 import './components/Carousel/Carousel.js'
+import './components/Footer/Footer.js'
